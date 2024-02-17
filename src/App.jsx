d183e8e9c774b2d1ee610b57e8805cfe37325352
VimalRaghunath <vimalvrk@gmail.com>
2024-02-17 18:37:01 +0530

    daily updation frontend

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className="App">
       <Routes>
-       <Route path='/' element={<HomePage/>} exact/>
-       <Route path='/chats' element={<ChatPage/>}/>
+       <Route path='/' element={<HomePage/>} exact />
+       <Route path='/chats' element={<ChatPage/>} />
       </Routes>
       
     </div>
