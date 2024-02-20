253f2723a331eca71a6ae414bb9b6da17976b219
VimalRaghunath <vimalvrk@gmail.com>
2024-02-20 10:08:37 +0530

    daily updation frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import ChatPage from './pages/ChatPage';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ChatPage from "./pages/ChatPage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-       <Route path='/' element={<HomePage/>} exact />
-       <Route path='/chats' element={<ChatPage/>} />
+        <Route path="/" element={<HomePage />} exact />
+        <Route path="/chats" element={<ChatPage />} />
       </Routes>
-      
     </div>
   );
 }
 
 export default App;
-
