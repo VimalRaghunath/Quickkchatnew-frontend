3fdb86ced52f97b767cb829d53abfd0e3374a072
VimalRaghunath <vimalvrk@gmail.com>
2024-03-12 21:16:07 +0530

    frontend profile modal and sidedrawer completed

--- src/pages/SideDrawer.jsx
+++ src/pages/SideDrawer.jsx
@@ -1,26 +1,75 @@
-import { Box, Button, Text, Tooltip } from '@chakra-ui/react';
-import React, { useState } from 'react'
+import {
+  Avatar,
+  Box,
+  Button,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
+  Text,
+  Tooltip,
+} from "@chakra-ui/react";
+import { BellIcon, ChevronDownIcon } from "@chakra-ui/icons";
+import React, { useState } from "react";
+import { ChatState } from "../Components/Authentication/Context/ChatProvider";
+import ProfileModal from "./ProfileModal";
 
 function SideDrawer() {
-
-  const [search, setSearch] = useState("")
+  const [search, setSearch] = useState("");
   const [searchResult, setSearchResult] = useState([]);
   const [loading, setLoading] = useState(false);
   const [loadingChat, setLoadingChat] = useState();
+
+  const { user } = ChatState();
+
   return (
     <div>
-      <Box>
-        <Tooltip label="Search Users to Chat" hasArrow placement='bottom-end' >
+      <Box
+        display={"flex"}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+        bg={"skyblue"}
+        w={"100%"}
+        p={"5px 10px 5px 10px"}
+        borderWidth={"5px"}
+      >
+        <Tooltip label="Search Users to Chat" hasArrow placement="bottom-end">
           <Button variant={"ghost"}>
-          <i class="fa-solid fa-magnifying-glass"></i>
-          <Text display={{ base: "none", md: "flex" }}>
-            Search User
-          </Text>
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <Text display={{ base: "none", md: "flex" }} px={"4"}>
+              Search User
+            </Text>
           </Button>
         </Tooltip>
+        <Text fontSize={"2xl"} fontFamily={"Work sans"}>
+          QuickkChat
+        </Text>
+        <div>
+          <Menu>
+            <MenuButton p={1}>
+              <BellIcon fontSize={"2xl"} m={1} />
+            </MenuButton>
+          </Menu>
+          <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+              <Avatar size={"sm"} cursor={"pointer"} 
+              name={user.name}
+              src={user.pic}
+              />
+            </MenuButton>
+            <MenuList>
+              <ProfileModal user={user}>
+                {/* <MenuItem>My Profile</MenuItem> */}
+              </ProfileModal>
+              <MenuDivider/>
+                <MenuItem>Logout</MenuItem>
+            </MenuList>
+          </Menu>
+        </div>
       </Box>
     </div>
-  )
+  );
 }
 
-export default SideDrawer
+export default SideDrawer;
