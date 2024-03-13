91dcbd42d736f7458cfec181e7c7360e8598ad68
VimalRaghunath <vimalvrk@gmail.com>
2024-03-13 18:53:24 +0530

    users fetching to the homepage is not completed. still pending

--- src/pages/MyChats.jsx
+++ src/pages/MyChats.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import { ChatState } from '../Components/Authentication/Context/ChatProvider'
-import { Box, useToast } from '@chakra-ui/react';
+import { Box, Button, Stack, Text, useToast } from '@chakra-ui/react';
 import { AxiosInstance } from '../AxiosInstance/AxiosInstance';
+import { AddIcon } from '@chakra-ui/icons';
+import ChatLoading from './ChatLoading';
+import { getSender } from './Config/ChatLogic';
 
 const MyChats = () => {
 
@@ -44,11 +47,66 @@ const MyChats = () => {
      alignItems={"center"}
      p={3}
      bg={"white"}
-     w={{ base: "100%", md: "31%" }}
+     w={{ base: "100%", md: "30%" }}
      borderRadius={"1g"}
      borderWidth={"1px"}
+     
    >
-    
+
+     <Box
+       pb={3}
+       px={3}
+       fontSize={{ base: "28px", md: "30px" }}
+       fontFamily={"Work sans"}
+       display={"flex"}
+       width={"100%"}
+       justifyContent={"space-between"}
+       alignItems={"center"}
+     > 
+       My Chats
+       <Button 
+         display={"flex"} 
+         fontSize={{ base: "17px", md: "10px", lg:"17px" }} 
+         rightIcon={<AddIcon/>}
+         >
+        New Group Chat
+       </Button>
+     </Box>
+    <Box
+     display={"flex"}
+     flexDir={"column"}
+     p={3}
+     bg={"#F8F8F8"}
+     w={"100%"}
+     h={"100%"}
+     borderRadius={"1g"}
+     overflowY={"hidden"}
+    >
+      {chats ? (
+        <Stack overflowY={"scroll"}>
+          {chats.map((chat) => (
+             <Box
+              onClick={() => setSelectedChat(chat)}
+              cursor={"pointer"}
+              bg={selectedChat === chat ? "#38B2AC" : "#E8E8E8"}
+              color={selectedChat === chat ? "white" :"black" }
+              px={3}
+              py={2}
+              borderRadius={"1g"}
+              key={chat._id}
+             >
+               <Text>
+                {!chat.isGroupChat 
+                  ? getSender(loggedUser, chat.users) 
+                  : chat.chatName}
+               </Text>
+             </Box>
+          ))}
+        </Stack>
+      ) : (
+        <ChatLoading/>
+      )}
+    </Box>
    </Box>
   )
 }
