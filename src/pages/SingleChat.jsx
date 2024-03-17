c18c71ff7108d272e7bf69ea04491f59e65af003
VimalRaghunath <vimalvrk@gmail.com>
2024-03-17 19:41:32 +0530

    daily updation

--- src/pages/SingleChat.jsx
+++ src/pages/SingleChat.jsx
@@ -52,7 +52,7 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
                   flexDir={"column"}
                   justifyContent={"flex-end"}
                   p={3}
-                  bg={"E8E8E8"}
+                  bg={"lightyellow"}
                   w={"100%"}
                   h={"100%"}
                   borderRadius={"1g"}
