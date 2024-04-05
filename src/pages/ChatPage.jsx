b496c94629b728a0ab904378c215bd96bf345559
VimalRaghunath <vimalvrk@gmail.com>
2024-04-05 09:15:53 +0530

    daily updation

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -6,26 +6,27 @@ import MyChats from "./MyChats";
 import ChatBox from "./ChatBox";
 
 const ChatPage = () => {
-
-  const { user } = ChatState() ;
-  const [ fetchAgain, setFetchAgain ] = useState(false)
+  const { user } = ChatState();
+  const [fetchAgain, setFetchAgain] = useState(false);
 
   return (
-   <div style={{ width: "100%"}}>
-     {user && <SideDrawer/>}
-     <Box
-       display="flex"
+    <div style={{ width: "100%" }}>
+      {user && <SideDrawer />}
+      <Box
+        display="flex"
         alignItems="center"
         justifyContent="space-between"
         flexDirection="row"
         height="15vh"
         padding="10px"
-     >
-      {user && <MyChats fetchAgain={fetchAgain}  />}
-      {user && <ChatBox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />}
-     </Box>
-   </div>
-  )
+      >
+        {user && <MyChats fetchAgain={fetchAgain} />}
+        {user && (
+          <ChatBox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+        )}
+      </Box>
+    </div>
+  );
 };
 
 export default ChatPage;
