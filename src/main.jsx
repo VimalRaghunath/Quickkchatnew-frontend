b7d6d229eb9f053d6ffb811fc35c40e38c35dfa0
VimalRaghunath <vimalvrk@gmail.com>
2024-04-29 09:24:50 +0530

    daily updation

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
    <ChatProvider>
       <ChakraProvider>
-        <App />
+         <App />
       </ChakraProvider>
    </ChatProvider>,
   </BrowserRouter>
